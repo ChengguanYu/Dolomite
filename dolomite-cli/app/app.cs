5139999bbe68ea495aa00863ca95a71e0e332950
weiyc <csway1024@gmail.com>
2026-06-15 19:55:43 +0800

    init

---
+++ dolomite-cli/app/app.cs
@@ -0,0 +1,13 @@
+﻿namespace dolomite_cli;
+using Microsoft.Extensions.Configuration;
+
+public class App
+{
+    static void Main(string[] args)
+    {
+        IConfiguration configuration = new AppConfigure(args).Build();
+
+        string? greeting = configuration.GetValue<string>("Greeting");
+        Console.WriteLine(greeting ?? "Hello, World!");
+    }
+}
---
+++ dolomite-cli/app/configure/AppConfigure.cs
@@ -0,0 +1,23 @@
+﻿namespace dolomite_cli;
+using Microsoft.Extensions.Configuration;
+
+public class AppConfigure
+{
+    private readonly IConfiguration _configuration;
+
+    public AppConfigure(string[] args)
+    {
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddEnvironmentVariables()
+            .AddCommandLine(args, new Dictionary<string, string>
+            {
+                { "-g", "Greeting" },
+                { "-Greeting", "Greeting" },
+            })
+            .Build();
+    }
+
+    public IConfiguration Build() => _configuration;
+}
